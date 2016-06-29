06d90b69800c1e4c13aaa337c97061c03d5921d8
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2016-06-29 17:42:43 +0200

    IIS install script now removes default web sites and app pools

--- install.software/install.iis.ps1
+++ install.software/install.iis.ps1
@@ -1,7 +1,7 @@
 # List of components http://stackoverflow.com/questions/37892173/automating-installation-of-iis/37893187#37893187
 # If script is running on Windows Server
 if ((Get-WmiObject Win32_OperatingSystem).Name -like '*server*') {
-	Write-Host "Identified host as Windows Server"
+	Write-Host "Identified host as Windows Server" -ForegroundColor Yellow
 	Import-Module ServerManager
 	# If this breaks because you don't have c:\sources\sxs you can't install Web-Asp-Net and Web-Net-Ext
 	# In that case remove the -Source argument and those two components from the list
@@ -26,7 +26,7 @@ if ((Get-WmiObject Win32_OperatingSystem).Name -like '*server*') {
 # If script is running on regular Windows
 # This does not install NET 3.5
 else {
-	Write-Host "Identified host as Windows Standard"
+	Write-Host "Identified host as Windows Standard" -ForegroundColor Yellow
 	Enable-WindowsOptionalFeature -Online -NoRestart -FeatureName `
 		IIS-Webserver,
 		IIS-HttpRedirect,
@@ -51,11 +51,25 @@ else {
 		`
 }
 
+Write-Host "Adding Windows features" -ForegroundColor Yellow
 Add-WindowsFeature NET-HTTP-Activation
 Add-WindowsFeature NET-WCF-HTTP-Activation45
 Add-WindowsFeature NET-WCF-MSMQ-Activation45
 Add-WindowsFeature NET-WCF-Pipe-Activation45
 Add-WindowsFeature NET-WCF-TCP-Activation45
 
+Write-Host "Removing standard websites and application pools" -ForegroundColor Yellow
+Try {
+  Remove-Website "Default Web Site"
+} Catch {}
+Try {
+  Remove-WebAppPool ".NET v2.0"
+  Remove-WebAppPool ".NET v2.0 Classic"
+  Remove-WebAppPool ".NET v4.5"
+  Remove-WebAppPool ".NET v4.5 Classic"
+  Remove-WebAppPool "Classic .NET AppPool"
+  Remove-WebAppPool "DefaultAppPool"
+} Catch {}
+
 # List installed components
-Get-WindowsOptionalFeature -online | Where {$_.FeatureName -like 'IIS*'} | Sort FeatureName | Format-Table
+# Get-WindowsOptionalFeature -online | Where {$_.FeatureName -like 'IIS*'} | Sort FeatureName | Format-Table
