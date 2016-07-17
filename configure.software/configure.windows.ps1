c6876ef7748e2b5e42b582a3b8dcef0d153b0d85
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2016-07-17 21:43:40 +0200

    Added configure windows script

---
+++ configure.software/configure.windows.ps1
@@ -0,0 +1,18 @@
+﻿# Set pinned items
+# Install-ChocolateyPinnedTaskBarItem "$($Boxstarter.programFiles86)\Google\Chrome\Application\chrome.exe"
+
+Update-ExecutionPolicy Unrestricted
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions -EnableShowFullPathInTitleBar -EnableOpenFileExplorerToQuickAccess -EnableShowRecentFilesInQuickAccess -EnableShowFrequentFoldersInQuickAccess -EnableExpandToOpenFolder
+Enable-RemoteDesktop
+Disable-InternetExplorerESC
+Disable-GameBarTips
+Disable-UAC
+Set-TaskbarOptions -Size Small -Lock -Dock Top -Combine Always
+Install-WindowsUpdate -Full
+
+# Edit registry
+cd $PSScriptRoot
+regedit /s ../reg/ChangeCapsLockToControl.reg
+
+# Import Start Menu Layout (Export using Export-StartLayout -Path C:\StartMenuLayout.xml)
+Import-StartLayout –LayoutPath $PSScriptRoot\..\xml\StartMenuLayout.xml -MountPath $env:SystemDrive\
