22e23892fd4c8c302597e1dc05111768b59cfe7f
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2016-07-18 23:19:39 +0200

    Added urlrewrite

--- install.software/choco.professional.ps1
+++ install.software/choco.professional.ps1
@@ -4,6 +4,7 @@
 # Install-ChocolateyVsixPackage StopOnFirstBuildError http://visualstudiogallery.msdn.microsoft.com/91aaa139-5d3c-43a7-b39f-369196a84fa5/file/44205/3/StopOnFirstBuildError.vsix
 # Install-ChocolateyVsixPackage VSCommandsForVisualStudio2013 https://visualstudiogallery.msdn.microsoft.com/c6d1c265-7007-405c-a68b-5606af238ece/file/106247/22/SquaredInfinity.VSCommands.VS12.vsix
 choco install -y resharper
+choco install -y urlrewrite
 choco install -y sourcetree
 choco install -y linqpad
 choco install -y fiddler
