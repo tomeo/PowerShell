b69c47996e71bd594abda847c92a860c7f7fbcb0
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2016-07-18 23:19:54 +0200

    Added git configuration

---
+++ configure.software/configure.git.ps1
@@ -0,0 +1,7 @@
+﻿git config --global user.name "Tommy Ivarsson"
+git config --global user.email "tommy.ivarsson@gmail.com"
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.lola "log --graph --decorate --pretty=oneline --abbrev-commit --all"
